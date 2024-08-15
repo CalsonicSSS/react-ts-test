a219a8774f4f599630ffc59dc52794967cdbc9c5
CalsonicSKC1005 <calson@withautomatic.com>
2024-08-14 21:11:57 -0400

    feature branch comments updated

--- src/App.tsx
+++ src/App.tsx
@@ -81,7 +81,7 @@ function App(): ReactElement {
     // Under context provider, the "value" can be shared, accessed, and used directly by any child component under it WITHOUT NEEDING TO USE "PROPS" TO PASS DOWN through each vertical level
     // the "value" prop here must match with the type annotation we defined previously in context file (very important)
     // the "value" prop here is the shared state value + its update function that we want to share with all child components under this context provider (with updated commits)
-    // This is feature-1-branch comments
+    // This is feature-1-branch comments (updated)
     <GlobalStateContext.Provider value={{ parentGlobalState, setParentGlobalState }}>
       <div>
         {/* {} are used to call js expression directly into jsx code | the expression return value will auto display on screen from jsx code */}
